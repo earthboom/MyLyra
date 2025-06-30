2aede4cc9505b89200618619be2fd1bdc94ec69f
earthboom <earthboom@naver.com>
2025-07-01 00:06:37 +0900

    CommonUser 2025.07.01

---
+++ Plugins/CommonUser/Source/CommonUser/Public/CommonSessionSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CommonSessionSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMONUSER_API UCommonSessionSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
